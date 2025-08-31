38f11e0e7377e173e880523c549c27d9f92e4e42
João Vasques <joaovasques.dev@gmail.com>
2025-08-31 16:22:32 -0300

    feat: Initial commit

---
+++ sdk/src/controllers/chatBotController.js
@@ -0,0 +1,98 @@
+// @ts-check
+
+/**
+ * @typedef {import("../views/chatBotView.js").ChatbotView} ChatBotView
+ * @typedef {import("../services/promptService.js").PromptService} PromptService
+ */
+
+export class ChatbotController {
+
+    #chatbotView;
+    #promptService;
+
+    /**
+    * @param {Object} deps - Dependencies for the class.
+    * @param {ChatBotView} deps.chatbotView - The chatbot view instance.
+    * @param {PromptService} deps.promptService - The prompt service instance.
+    */
+    constructor({ chatbotView, promptService }) {
+        this.#chatbotView = chatbotView;
+        this.#promptService = promptService;
+    }
+
+    async init({ firstBotMessage, text }) {
+        this.#setupEvents();
+        this.#chatbotView.renderWelcomeBubble();
+        this.#chatbotView.setInputEnabled(true);
+        this.#chatbotView.appendBotMessage(firstBotMessage, null, false);
+    }
+
+    #setupEvents() {
+        this.#chatbotView.setupEventHandlers({
+            onOpen: this.#onOpen.bind(this),
+            onSend: this.#chatBotReply.bind(this),
+            onStop: this.#handleStop.bind(this),
+        });
+    }
+
+    #handleStop() {
+    }
+
+    async #chatBotReply(userMsg) {
+        console.log('received', userMsg)
+        this.#chatbotView.showTypingIndicator();
+        this.#chatbotView.setInputEnabled(false);
+        setTimeout(() => {
+            this.#chatbotView.appendBotMessage("Opa! Ainda não estou pronto para isso.", null, false);
+            this.#chatbotView.setInputEnabled(true);
+            this.#chatbotView.hideTypingIndicator();
+        }, 500);
+
+    }
+
+    async #onOpen() {
+        const errors = this.#checkRequirements()
+
+        if (errors.length) {
+            const messages = errors.join('\n\n')
+
+            this.#chatbotView.appendBotMessage(
+                messages
+            )
+
+            this.#chatbotView.setInputEnabled(false);
+            return
+        }
+        
+        this.#chatbotView.setInputEnabled(true);
+    }
+
+    #checkRequirements() {
+        const errors = []
+        // @ts-ignore
+        const iChrome = window.chrome
+
+        if (!iChrome) {
+            errors.push(
+                'Este recurso só funciona no Google Chrome!'
+            );
+        }
+
+        if (!('LanguageModel' in window)) {
+            errors.push(
+                "As APIs nativas de IA não estão ativas."
+            );
+            errors.push(
+                "Ative a seguinte flag em chrome://flags/:"
+            );
+            errors.push(
+                "- Prompt API for Gemini Nano (chrome://flags/#prompt-api-for-gemini-nano)"
+            );
+            errors.push(
+                "Depois reinicie o Chrome e tente novamente."
+            );
+        }
+
+        return errors
+    }
+}
